13e8c81411eae1fd03ec7f05c92d2637e56f05c8
jonathan casagrande <casagrandesoaresj@gmail.com>
2021-09-18 19:03:11 -0300

    new script to find keywords on main title, done in 15 minutes without any bug

---
+++ findOnTitle.js
@@ -0,0 +1,113 @@
+const cheerio = require("cheerio");
+const { first } = require("cheerio/lib/api/traversing");
+const fs = require("fs");
+const path = require("path");
+const { moveMessagePortToContext } = require("worker_threads");
+const folderToBeScrapped = "./toBeScrapped/";
+const englishFolder = "./withEnglish/";
+profilesToBeScraped = [];
+
+async function everything() {
+	
+	await saveAllFilesToArray(); // this will read all the files in the folder and save it to the array, to be scraped afterwards.
+
+	// documentation below
+
+	async function saveAllFilesToArray() {
+		fs.readdir(folderToBeScrapped, (err, files) => {
+			files.forEach((file) => {
+				profilesToBeScraped.push(file);
+				//   console.log(file); // unit testing
+			});
+
+			/*
+				i couldn't manage to save this array to the memory, that's why you have this code inside the FS.READDIR
+			*/
+			
+			// this will loop the list of profiles to be scraped
+			for (let i = 0; i < profilesToBeScraped.length; i++) {
+				let currentProfile = profilesToBeScraped[i];
+
+				// this will READ the looped file and apply the english filters
+				fs.readFile(folderToBeScrapped + currentProfile, "utf8", (err, html) => {
+					if (err) {
+						console.error(err);
+						return;
+					}
+
+					let $ = cheerio.load(html);
+					let wholeHTML = $("body").text();
+
+					let title = $('.text-body-medium.break-words').text();
+
+					if (title.match(/ qualidade/gim)) {moveToEnglishFolder() ; console.log(title)} 
+						else if (title.match(/ test/gim)) {moveToEnglishFolder() ; console.log(title)}
+						else if (title.match(/ automa/gim)) {moveToEnglishFolder() ; console.log(title)}
+						else if (title.match(/ qa/gim)) {moveToEnglishFolder() ; console.log(title)}
+						else if (title.match(/ quality/gim)) {moveToEnglishFolder() ; console.log(title)}
+						else if (title.match(/ sdet/gim)) {moveToEnglishFolder() ; console.log(title)}
+
+
+					// this is the code to change the folder's directory
+					async function moveToEnglishFolder() {
+						const currentPath = path.join(
+							__dirname,
+							folderToBeScrapped,
+							currentProfile
+						);
+						const destinationPath = path.join(
+							__dirname,
+							englishFolder,
+							currentProfile
+						);
+
+						fs.rename(currentPath, destinationPath, function (err) {
+							if (err) {
+								throw err;
+							} else {
+								//     console.log("Successfully moved the file!"); // unit testing
+							}
+						});
+					}
+
+					// console.log((wholeHTML.match(/inglês/g) || []).length)
+
+					// if () {}
+
+					// proximo passo a pensar:
+					// como evito copiar linhas de codigo mudando apenas o regex?
+
+					// if (body.match(/ingles/g) || []) {}
+
+					// fs.writeFile("test2.txt", firstText, function (err) { if (err) { console.log(err); } });
+				});
+			}
+		});
+	}
+
+}
+
+everything();
+
+/*
+
+//PROFILE PARTS
+
+// BIO
+document.querySelector('[class="pv-profile-section__card-header"]').nextElementSibling
+
+// position title
+document.querySelectorAll('[class="pv-entity__secondary-title t-14 t-black t-normal"]')[0].previousElementSibling.previousElementSibling.innerText
+
+// experience time
+document.querySelectorAll('[class="pv-entity__bullet-item-v2"]')[0]
+
+// experiences bio
+document.querySelectorAll('[class="pv-entity__extra-details t-14 t-black--light ember-view"]')[0]
+
+// languages
+document.querySelector('[class="pv-accomplishments-block__list-container"]').children[0].children[0]
+
+// when i is the english element at unknown position
+document.querySelector('[class="pv-accomplishments-block__list-container"]').children[0].children[i].children[1].innerText
+*/
