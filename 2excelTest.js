9c1432899179369d64e2114159201cec20e42c3b
Jonathan <you@example.com>
2021-09-27 01:34:01 -0300

    lot of progress

--- 2excelTest.js
+++ 2excelTest.js
@@ -44,26 +44,87 @@ async function initialize() {
                     let rawURL = $(".ember-view.link-without-visited-state.cursor-pointer.text-heading-small.inline-block.break-words").attr("href");
                     url = rawURL.split("detail")[0];
 
-                    //     URL: url,
-                    //     dateOfEntry: dateOfEntry,
-                    //     name: candidateName,
-                    //     location: location,
-                    //     title: profileTitle,
+                    // get current date
+                    var today = new Date();
+                    var dd = String(today.getDate()).padStart(2, "0");
+                    var mm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
+                    var yyyy = today.getFullYear();
+                    dateOfEntry = dd + "/" + mm + "/" + yyyy;
 
-                    //     // experiences
-                    //     let experiencesLIs = $("li.pv-entity__position-group-pager.pv-profile-section__list-item.ember-view");
+                    // get candidate name
+                    candidateName = $(".text-heading-xlarge.inline.t-24.v-align-middle.break-words").text();
 
-                    //     for (let i = 0; i < experiencesLIs.length; i++) {
-                    //       let loopedExperience = $("li.pv-entity__position-group-pager.pv-profile-section__list-item.ember-view").eq(i);
+                    // get location
+                    rawLocation = $(".text-body-small.inline.t-black--light.break-words").text();
+                    location = rawLocation.trim();
 
-                    //       wordCounter = (loopedExperience.match(/java/igm) || []).length
+                    // profile title
+                    rawProfileTitle = $(".text-body-medium.break-words").text();
+                    profileTitle = rawProfileTitle.trim();
 
-                    //       try { if (wordCounter > 0) { moveTotechnicalFolder() } } catch (error) {console.log('1')}
+                    // connectionState
+                    connectionStateButton = $(".pvs-profile-actions ").children().eq(1).text();
 
-                    //     }
+                    if (connectionStateButton.match(/Conectar/gim)) {
+                        connectionState = "não conectado";
+                    } else if (connectionStateButton.match(/Seguir/gim)) {
+                        connectionState = "inconectável";
+                    } else if (connectionStateButton.match(/enviar/gim)) {
+                        connectionState = "conectado";
+                    }
 
-                    
-                    
+                    // currentCompany
+                    rawCurrentCompany = $("h3.t-16.t-black.t-bold").eq(0).children().text();
+                    currentCompany = rawCurrentCompany.replace(/Nome da empresa/gim, "");
+                    console.log(currentCompany.trim());
+
+                    // yearsOfExperience
+
+                    // xSenior
+                    wholeHTML = $("body").text();
+
+                    // PENDING
+
+
+
+                    /*
+
+                        COMPANY NAME
+
+                        document.querySelector('[class="pv-entity__position-group-pager pv-profile-section__list-item ember-view"]').children().eq(0).children().eq(0).children().eq(0).children().eq(0).children().eq(1).children().eq(2)
+
+                        ROLE AT EXP
+
+                        document.querySelector('[class="pv-entity__position-group-pager pv-profile-section__list-item ember-view"]').children().eq(0).children().eq(0).children().eq(0).children().eq(0).children().eq(1).children().eq(0)
+
+
+                        DOUBLE EXP TIME
+
+                        document.querySelector('[class="pv-entity__position-group-pager pv-profile-section__list-item ember-view"]').children().eq(0).children().eq(0).children().eq(0).children().eq(0).children().eq(1).children().eq(1)
+
+
+                        BETTER TIME
+
+                        document.querySelector('[class="pv-entity__position-group-pager pv-profile-section__list-item ember-view"]').children().eq(0).children().eq(0).children().eq(0).children().eq(0).children().eq(1).children().eq(3).children().eq(1)
+
+                   */
+
+                    // xNode: xnode,
+                    // xJava: xjava,
+                    // xNet: xnet,
+
+                    // experiences
+                    // let experiencesLIs = $("li.pv-entity__position-group-pager.pv-profile-section__list-item.ember-view");
+
+                    // for (let i = 0; i < experiencesLIs.length; i++) {
+                    // let loopedExperience = $("li.pv-entity__position-group-pager.pv-profile-section__list-item.ember-view").eq(i);
+
+                    // wordCounter = (loopedExperience.match(/java/igm) || []).length
+
+                    // try { if (wordCounter > 0) { moveTotechnicalFolder() } } catch (error) {console.log('1')}
+
+                    // }
+
                     // send data to xlsx
                     function updateSheet() {
                         worksheets.Sheet1.push({
@@ -71,12 +132,12 @@ async function initialize() {
                             // Country: {},
 
                             URL: url,
-                            // dateOfEntry: dateOfEntry,
-                            // name: candidateName,
-                            // location: location,
-                            // title: profileTitle,
-                            // connectionState: connectionState,
-                            // currentCompany: currentCompany,
+                            dateOfEntry: dateOfEntry,
+                            name: candidateName,
+                            location: location,
+                            title: profileTitle,
+                            connectionState: connectionState,
+                            currentCompany: currentCompany,
                             // yearsOfExperience: yearsOfExperience,
                             // xSenior : xsenior,
                             // xNode: xnode,
@@ -156,10 +217,31 @@ async function initialize() {
                 // throw err;
                 1 + 1;
             } else {
-                //     console.log("Successfully moved the file!"); // unit testing
+                // console.log("Successfully moved the file!"); // unit testing
             }
         });
     }
 }
 
 initialize();
+
+/*
+
+// BIO
+document.querySelector('[class="pv-profile-section__card-header"]').nextElementSibling
+
+// role title
+document.querySelectorAll('[class="pv-entity__secondary-title t-14 t-black t-normal"]')[0].previousElementSibling.previousElementSibling.innerText
+
+// experience time
+document.querySelectorAll('[class="pv-entity__bullet-item-v2"]')[0]
+
+// experiences bio
+document.querySelectorAll('[class="pv-entity__extra-details t-14 t-black--light ember-view"]')[0]
+
+// languages
+document.querySelector('[class="pv-accomplishments-block__list-container"]').children[0].children[0]
+
+// when i is the english element at unknown position
+document.querySelector('[class="pv-accomplishments-block__list-container"]').children[0].children[i].children[1].innerText
+*/
