45f6f8a9b23292f497b10eacd298693acea7d16c
jonathan casagrande <casagrandesoaresj@gmail.com>
2021-11-07 22:56:54 -0300

    its working, but needs polishing

--- modules/titleAndTime.js
+++ modules/titleAndTime.js
@@ -5,7 +5,7 @@ const path = require("path");
 const { moveMessagePortToContext } = require("worker_threads");
 const folderToBeScrapped = "./zprofiles/1english/node/";
 const techFolder = "./techFolder/";
-profileToBeScraped = 'D:/Users/jonat/git/l1nk3d1n/zprofiles/1english/(1) ☕ Everton (Tom) Costa _ LinkedIn.html';
+profileToBeScraped = 'D:/Users/jonat/git/l1nk3d1n/zprofiles/1english/(2) Fabiana Viana _ LinkedIn.html';
 
 fs.readFile(profileToBeScraped, "utf8", (err, html) => {
     if (err) {
@@ -16,18 +16,26 @@ fs.readFile(profileToBeScraped, "utf8", (err, html) => {
     let $ = cheerio.load(html);
     let wholeHTML = $("body").text();
 
-    let allExperiences = $('li.pv-entity__position-group-pager.pv-profile-section__list-item.ember-view')
+    let experiencesList = $('li.pv-entity__position-group-pager.pv-profile-section__list-item.ember-view')
 
-    for (let i = 0; i < allExperiences.length; i++) {
+    // get title and time for all grouped experiences first
+    // try { let groupedExperiencesList = $('pv-entity__position-group-role-item') } catch (error) { console.log(error) }
 
-        // access exp title's text
-        let allExperiencesTitle = allExperiences.eq(i).children().children().children().children().children().eq(1).children().eq(0).text()
+    for (let i = 0; i < experiencesList.length; i++) {
+        let loopedExperienceText = experiencesList[i]
+        console.log(loopedExperienceText)
 
-        // access exp total time
-        let allExperienceTotalTime = allExperiences.eq(i).children().children().children().children().children().eq(1).children().children().children().eq(3).text()
+            // access exp title's text
+            let loopedExperienceTitle = experiencesList.eq(i).children().children().children().children().children().eq(1).children().eq(0).text()
+ 
+            // access exp total time
+            let loopedExperiencesTotalTime = experiencesList.eq(i).children().children().children().children().children().eq(1).children().children().children().eq(3).text()
 
-        fs.appendFile('./output.txt', allExperienceTotalTime + '\n', function (error) { if (error) {console.log(error)} })
-
+            fs.appendFile('./log.txt', loopedExperienceTitle + '\n', function (error) { if (error) { console.log(error) } })
+            fs.appendFile('./log.txt', loopedExperiencesTotalTime + '\n', function (error) { if (error) { console.log(error) } })
+            fs.appendFile('./log.txt', '\n\n', function (error) { if (error) { console.log(error) } })
+            
+            
     }
 
 });
