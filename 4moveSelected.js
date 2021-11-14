453da1ba42cf32fd15a2cf5fad651fb2c011ea84
jonathan casagrande <casagrandesoaresj@gmail.com>
2021-11-14 20:26:00 -0300

    little adjustments

--- 4moveSelected.js
+++ 4moveSelected.js
@@ -2,13 +2,32 @@ const fs = require("fs");
 const path = require("path");
 const {first} = require("cheerio/lib/api/traversing");
 const {moveMessagePortToContext} = require("worker_threads");
-const folderToBeScrapped = "./zprofiles/1english/";
-const selectedFolder = "./selectedFolder/";
 
-// const folderToBeScrapped = "./selectedFolder/";
-// const selectedFolder = "./selectedFolder/good/";
+// const folderToBeScrapped = "./zprofiles/1english/";
+// const selectedFolder = "./selectedFolder/";
 
-profilesToBeMoved = [];
+const folderToBeScrapped = "./selectedFolder/eval/";
+const selectedFolder = "./selectedFolder/good/";
+
+profilesToBeMoved = [
+    'a (509).html',
+    '(1) Augusto Kern _ LinkedIn.html',
+    '(1) Gustavo Coelho _ LinkedIn.html',
+    `(1) Leonardo Maia D'Avila Martins _ LinkedIn.html`,
+    '(1) Oggo Petersen _ LinkedIn.html',
+    'a (205).html',
+    'a (502).html',
+    'a (618).html',
+    'a (1251).html',
+    'a (1915).html',
+    'a (1515).html',
+    'evdevops (20).html',
+    'a (1925).html',
+    'a (2875).html',
+    'a (3014).html',
+    'a (3079).html',
+    
+];
 
 async function initialize() {
     // this will loop the list of profiles to be scraped
